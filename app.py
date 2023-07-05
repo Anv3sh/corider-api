14a49edc0cb377765e22a0406eca8e8582212bc5
= <=>
2023-07-05 23:12:30 +0530

    migrated to flask_restful

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/Users'
 mongo = PyMongo(app)
+api = Api(app)
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    new_user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    result = mongo.db.user.insert_one(new_user)
-    return jsonify({'id': str(result.inserted_id), 'name': new_user['name'], 'email': new_user['email']}), 201
+# User resource
+class UserResource(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            user = mongo.db.user.find_one_or_404({'_id': ObjectId(user_id)}, {'password': 0})
+            return {'id': str(user['_id']), 'name': user['name'], 'email': user['email']}
+        else:
+            users = mongo.db.user.find({}, {'password': 0})
+            return [{'id': str(user['_id']), 'name': user['name'], 'email': user['email']} for user in users]
 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        data = parser.parse_args()
+        new_user = {
+            'name': data['name'],
+            'email': data['email'],
+            'password': data['password']
+        }
+        result = mongo.db.user.insert_one(new_user)
+        return {'id': str(result.inserted_id), 'name': new_user['name'], 'email': new_user['email']}, 201
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = mongo.db.user.find({}, {'password': 0}) 
-    return jsonify([{'id': str(user['_id']), 'name': user['name'], 'email': user['email']} for user in users])
+    def put(self, user_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        data = parser.parse_args()
+        updated_user = {
+            'name': data['name'],
+            'email': data['email'],
+            'password': data['password']
+        }
+        result = mongo.db.user.update_one({'_id': ObjectId(user_id)}, {'$set': updated_user})
+        if result.modified_count == 0:
+            return {'error': 'User not found'}, 404
+        return {'id': user_id, 'name': updated_user['name'], 'email': updated_user['email']}
 
+    def delete(self, user_id):
+        result = mongo.db.user.delete_one({'_id': ObjectId(user_id)})
+        if result.deleted_count == 0:
+            return {'error': 'User not found'}, 404
+        return {'message': 'User deleted'}
 
-@app.route('/users/<string:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = mongo.db.user.find_one_or_404({'_id': ObjectId(user_id)}, {'password': 0}) 
-    return jsonify({'id': str(user['_id']), 'name': user['name'], 'email': user['email']})
-
-@app.route('/users/<string:user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.get_json()
-    updated_user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    result = mongo.db.user.update_one({'_id': ObjectId(user_id)}, {'$set': updated_user})
-    if result.modified_count == 0:
-        return jsonify({'error': 'User not found'}), 404
-    return jsonify({'id': user_id, 'name': updated_user['name'], 'email': updated_user['email']})
-
-@app.route('/users/<string:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    result = mongo.db.user.delete_one({'_id': ObjectId(user_id)})
-    if result.deleted_count == 0:
-        return jsonify({'error': 'User not found'}), 404
-    return jsonify({'message': 'User deleted'})
+# Add resource routes
+api.add_resource(UserResource, '/users', '/users/<string:user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
